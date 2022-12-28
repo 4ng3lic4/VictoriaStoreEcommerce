74002480f49d742895dbff94f45058dfbd95d105
4ng3lic4 <ositosviajeros@hotmail.com>
2022-12-27 22:13:06 -0500

    defining columns and adding initial files

---
+++ models/Category.js
@@ -0,0 +1,37 @@
+const { STRING } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    
+    // * `id`
+    id: {
+      // * Integer.
+      type: DataTypes.INTEGER
+      // * Doesn't allow null values.
+      allowNull: false,
+      // * Set as primary key.
+    primaryKey: true,
+      // * Uses auto increment.
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes-STRING,
+      allowNull:false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
