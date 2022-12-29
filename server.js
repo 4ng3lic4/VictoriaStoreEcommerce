7d7c3a646d4615fd03e97491b00b3668c0d5cafc
4ng3lic4 <ositosviajeros@hotmail.com>
2022-12-28 19:25:34 -0500

    imported sequelize connection to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,10 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-
 /** //dont forget to do the synch (force falso, true in activity 14 )
 /* See Activity 14
-
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
